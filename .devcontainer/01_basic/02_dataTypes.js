9215295cdfcecaf68feecefc95950313be1aa45e
Shaik Usman <86777263+usmansk1223@users.noreply.github.com>
2024-03-13 16:23:46 +0000

    dataTypes

---
+++ .devcontainer/01_basic/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict" // treat all JS code as newer version
+/* we've got older version of JavaScirpt, 
+JS coummity build newer version of JS which is  object, classes and modules etc 
+to work with newer version we define use Strict syntax that it's we're write code in newer version */
+
+// alert(3+3) // we are using nodejs, not browser
+
+//console.log(3+3) console.log(Usman) //this is bad practice and js will through an error
+console.log("Usman"); console.log(3+3) // best practice but still to use one console per line
+console.log(3
+    +3);//bad practice //code readability should be high
+console.log("Usman");    
+
+let name = "Usman"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint => used for big numbers 
+// string => "" ''
+// boolean => ture or false
+// null  // standalone value
+// undefined => 
+// symbol => unique 
+
+// object
+
+console.log(typeof "Usman")
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof true);
