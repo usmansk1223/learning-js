c9c77d00c5c9220645bdfa4002a18dc52d594bf3
Shaik Usman <86777263+usmansk1223@users.noreply.github.com>
2024-03-13 07:09:05 +0000

    Started learning variables in JS

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 144235
+/*This const keyword it is a variable type and value we assign to this will remain constant we can't
+redeclare the value
+*/
+let accountEmail = "usman@google.com"
+/* This is variable here we can change the assigned value and it store in memory as container
+This is new way of declaring variable and it's resolve the issues of block scope and functional scope */
+var  accountPassword = "12345"
+// try not to use this variable bacause it creates issues in block scope and functional scope
+accountCity = "Hyderabad"
+
+//accountId = 4 // not allowed 
+/* Here below we're reassigning the value and worked in console as expected*/
+accountEmail = "us@us.com"
+accountPassword = "23232323"
+accountCity = "Bengaluru"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
